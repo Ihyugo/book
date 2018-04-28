69e4f8efd61e254cefdfe0afda81a5e03c3277b8
yugo ikuta <yugo1997.10.14@gmail.com>
2018-04-28 19:52:48 +0900

    no

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def index 
-	  render html:"hello wold"
+	  agent = Mechanize.new
+	  page = agent.get("https://www.sciencenews.org/")
+	  elements = pages.search('//h2[@class="node-title"]')
+	  contents = []
+	  elements.each do |ele|
+		  methods = []
+		  methods.push(ele.push('a').get_attrubute('title'))
+		  methods.push(ele.push('a').get_attrubute('href'))
+		  contents.push(methods)
+	  end
+	  @title = contents
+
+			       
   end
 end
--- app/controllers/article_documents_controller.rb
+++ app/controllers/article_documents_controller.rb
@@ -1,5 +1,13 @@
 class ArticleDocumentsController < ApplicationController
   def index
 	  @index = "there are no index"
+	  content = []
+	  f = File.open('file/sample.odt','r')
+	  f.each do |text|
+		  content.push(text)
+	  end
+	  f.close()
+	  document = content.join
+	  @content = document
   end
 end
