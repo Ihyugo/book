1f893ed2cef3db85dea67cf917767216098872ba
yugo ikuta <yugo1997.10.14@gmail.com>
2018-04-29 12:35:20 +0900

    delete file

--- app/controllers/article_documents_controller.rb
+++ app/controllers/article_documents_controller.rb
@@ -2,31 +2,34 @@ class ArticleDocumentsController < ApplicationController
   def index
 	  if params[:root].nil?
 	  @index = "there are no index"
-	  content = []
-	  f = File.open('file/sample.odt','r')
-	  f.each do |text|
-		  content.push(text)
-	  end
-	  f.close()
-	  document = content.join
-	  @content = document
 	  else
 	  url = "https://www.sciencenews.org/" + params[:root]
-	  agent = Mechanize.new
-	  page= agent.get(url)
-	  page = page.search('//span[@itemprop="description"]')
-	  documents = page.search('p')
+	  documents = make_text(url)
 	  contents = []
 	  documents.each do |document|
 		  contents.push(document)
 		  contents.push("\n\n")
 	  end
 	  contents = contents.join
+	  if ArticleFile.find_by(title: params[:title]).nil?
+		  save_text(contents)
+	  end
+	  @content = contents
+	  end
+  end
+
+  def make_text(url)
+	  agent = Mechanize.new
+	  page= agent.get(url)
+	  page = page.search('//span[@itemprop="description"]')
+	  documents = page.search('p')
+	  return documents
+  end
+
+  def save_text(contents)
 	  @newfile = ArticleFile.new
 	  @newfile.title = params[:title]
 	  @newfile.text = contents
 	  @newfile.save
-	  @content = contents
-	  end
   end
 end
